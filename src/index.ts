747af1096306178a6bb59248e5dad45e60c3890b
Jesse McLean <essejmclean@gmail.com>
2022-02-10 09:59:17 -0700

    fix: export ClassProp interface

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ export const cx = <T extends CxOptions>(...classes: T): CxReturn =>
 /* cva
   ============================================ */
 
-interface ClassProp {
+export interface ClassProp {
   class?: ClassValue;
 }
 
