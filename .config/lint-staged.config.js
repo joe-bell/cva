f8462a176913120cf195bdf0bad4ed720057c727
joe-bell <joe-bell@users.noreply.github.com>
2022-06-11 11:08:45 -0600

    chore: prevent prettier error

--- .config/lint-staged.config.js
+++ .config/lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   "*": (filenames) =>
-    `prettier --ignore-unknown --write ${filenames
+    `prettier --ignore-unknown --no-error-on-unmatched-pattern --write ${filenames
       .map((filename) => `'${filename}'`)
       .join(" ")}`,
   "src/**/*.{js,jsx,ts,tsx}": () => "npm run check:tsc",
