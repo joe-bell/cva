5b978b2cc9cf19d435983349881d6d6a3a1ab1b1
joe-bell <joe-bell@users.noreply.github.com>
2022-07-15 09:35:47 +0700

    fix: amend lint-staged typecheck script

--- .config/lint-staged.config.mjs
+++ .config/lint-staged.config.mjs
@@ -3,5 +3,5 @@ export default {
     `prettier --ignore-unknown --no-error-on-unmatched-pattern --write ${filenames
       .map((filename) => `'${filename}'`)
       .join(" ")}`,
-  "src/**/*.{js,jsx,ts,tsx}": () => "npm run check:tsc",
+  "src/**/*.{js,jsx,ts,tsx}": () => "npm run lint:tsc",
 };
