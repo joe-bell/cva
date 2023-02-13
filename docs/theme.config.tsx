26f2408ce347ae2f0dfe2ef79586f9555bf826ff
Joe Bell <7349341+joe-bell@users.noreply.github.com>
2023-02-13 17:01:27 +0700

    chore: separate example pages

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -6,6 +6,17 @@ const config: DocsThemeConfig = {
   project: {
     link: "https://github.com/joe-bell/cva",
   },
+  feedback: {
+    useLink() {
+      const config = useConfig();
+
+      return `${
+        config.project.link
+      }/discussions/new?category=feedback&title=${encodeURIComponent(
+        `Feedback for "${config.title}"`
+      )}`;
+    },
+  },
   docsRepositoryBase: "https://github.com/joe-bell/cva/tree/main/docs",
   head: function useHead() {
     const { title } = useConfig();
