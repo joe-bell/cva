9ea99f82bf228aad6118afb0e756ff5ce404fec8
Joe Bell <7349341+joe-bell@users.noreply.github.com>
2023-02-14 14:51:49 +0700

    docs: add react with tailwind example

---
+++ examples/react-with-tailwind/src/app.tsx
@@ -0,0 +1,43 @@
+import { cx } from "class-variance-authority";
+import { Button } from "./components";
+
+const intents = [undefined, "primary", "secondary"] as const;
+const sizes = [undefined, "medium", "small"] as const;
+
+function App() {
+  return (
+    <table
+      className={cx(
+        "relative",
+        "h-max w-max",
+        "self-center justify-self-center",
+        "[&_:where(th,td)]:p-2"
+      )}
+    >
+      <thead>
+        <tr>
+          <th></th>
+          {intents.map((intent) => (
+            <th scope="col">{intent || "default"}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {sizes.map((size) => (
+          <tr>
+            <th scope="row">{size || "default"}</th>
+            {intents.map((intent) => (
+              <td scope="col">
+                <Button {...(intent && { intent })} {...(size && { size })}>
+                  {intent || "default"} button
+                </Button>
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default App;
---
+++ examples/react-with-tailwind/src/components/button/button.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+const button = cva("button", {
+  variants: {
+    intent: {
+      primary: [
+        "bg-blue-500",
+        "text-white",
+        "border-transparent",
+        "hover:bg-blue-600",
+      ],
+      secondary: [
+        "bg-white",
+        "text-gray-800",
+        "border-gray-400",
+        "hover:bg-gray-100",
+      ],
+    },
+    size: {
+      small: ["text-sm", "py-1", "px-2"],
+      medium: ["text-base", "py-2", "px-4"],
+    },
+  },
+  compoundVariants: [{ intent: "primary", size: "medium", class: "uppercase" }],
+  defaultVariants: {
+    intent: "primary",
+    size: "medium",
+  },
+});
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof button> {}
+
+export const Button: React.FC<ButtonProps> = ({
+  className,
+  intent,
+  size,
+  ...props
+}) => <button className={button({ intent, size, className })} {...props} />;
