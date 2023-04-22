3e7240b59b1af3de0f36df570589ddc2585b1697
Joe Bell <7349341+joe-bell@users.noreply.github.com>
2023-04-22 16:09:58 +0300

    chore: remove vercel analytics

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,12 +1,10 @@
 import type { AppProps } from "next/app";
-import { Analytics } from "@vercel/analytics/react";
 import "../styles/global.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
     </>
   );
 }
