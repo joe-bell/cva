5ef206fff065d56c4de865569bfd6fc9027a2433
Joe Bell <7349341+joe-bell@users.noreply.github.com>
2023-05-26 13:12:55 +0300

    chore: introduce interfaces

--- packages/class-variance-authority/src/index.ts
+++ packages/class-variance-authority/src/index.ts
@@ -1,6 +1,20 @@
 import clsx from "clsx";
 
-type ClassPropKey = "class" | "className";
+/* Utils
+  ============================================ */
+
+const falsyToString = <T extends unknown>(value: T) =>
+  typeof value === "boolean" ? `${value}` : value === 0 ? "0" : value;
+
+/* Types
+  ============================================ */
+
+/* clsx
+  ---------------------------------- */
+
+// When compiling with `declaration: true`, many projects experience the dreaded
+// TS2742 error. To combat this, we copy clsx's types manually.
+// Should this project move to JSDoc, this workaround would no longer be needed.
 
 type ClassValue =
   | ClassArray
@@ -21,17 +35,75 @@ type ClassProp =
   | { class?: never; className: ClassValue }
   | { class?: never; className?: never };
 
+/* Internal
+  ---------------------------------- */
+
+type ClassPropKey = "class" | "className";
 type OmitUndefined<T> = T extends undefined ? never : T;
 type StringToBoolean<T> = T extends "true" | "false" ? boolean : T;
 
-/* Utils
-  ============================================ */
+/* Public Utilities
+  ---------------------------------- */
 
-const falsyToString = <T extends unknown>(value: T) =>
-  typeof value === "boolean" ? `${value}` : value === 0 ? "0" : value;
+export type VariantProps<Component extends (...args: any) => any> = Omit<
+  OmitUndefined<Parameters<Component>[0]>,
+  ClassPropKey
+>;
 
-/* `create()`
-  ============================================ */
+/* cx
+  ---------------------------------- */
+
+interface CX {
+  (...inputs: ClassValue[]): string;
+}
+
+export type CXOptions = Parameters<CX>;
+export type CXReturn = ReturnType<CX>;
+
+/**
+ * @deprecated use `CXOptions`
+ */
+export type CxOptions = CXOptions;
+/**
+ * @deprecated use `CXReturn`
+ */
+export type CxReturn = CXReturn;
+
+/* cva
+  ---------------------------------- */
+
+interface CVA {
+  <T>(base?: ClassValue, config?: Config<T>): (props?: Props<T>) => string;
+}
+
+type ConfigSchema = Record<string, Record<string, ClassValue>>;
+
+type ConfigVariants<T extends ConfigSchema> = {
+  [Variant in keyof T]?: StringToBoolean<keyof T[Variant]> | null | undefined;
+};
+type ConfigVariantsMulti<T extends ConfigSchema> = {
+  [Variant in keyof T]?:
+    | StringToBoolean<keyof T[Variant]>
+    | StringToBoolean<keyof T[Variant]>[]
+    | undefined;
+};
+
+type Config<T> = T extends ConfigSchema
+  ? {
+      variants?: T;
+      defaultVariants?: ConfigVariants<T>;
+      compoundVariants?: (T extends ConfigSchema
+        ? (ConfigVariants<T> | ConfigVariantsMulti<T>) & ClassProp
+        : ClassProp)[];
+    }
+  : never;
+
+type Props<T> = T extends ConfigSchema
+  ? ConfigVariants<T> & ClassProp
+  : ClassProp;
+
+/* create
+  ---------------------------------- */
 
 interface CreateOptions {
   hooks?: {
@@ -39,110 +111,86 @@ interface CreateOptions {
   };
 }
 
-export const create = (options?: CreateOptions) => {
-  /* cx
-    ---------------------------------- */
+interface Create {
+  (options?: CreateOptions): {
+    cx: CX;
+    cva: CVA;
+  };
+}
 
-  const cx = (...inputs: ClassValue[]): string =>
+/* Exports
+  ============================================ */
+
+export const create: Create = (options) => {
+  const cx: CX = (...inputs) =>
     typeof options?.hooks?.["cx:done"] !== "undefined"
       ? options?.hooks["cx:done"](clsx(inputs))
       : clsx(inputs);
 
-  /* cva
-    ---------------------------------- */
+  const cva: CVA = (base, config) => (props) => {
+    if (config?.variants == null)
+      return cx(base, props?.class, props?.className);
 
-  type ConfigSchema = Record<string, Record<string, ClassValue>>;
+    const { variants, defaultVariants } = config;
 
-  type ConfigVariants<T extends ConfigSchema> = {
-    [Variant in keyof T]?: StringToBoolean<keyof T[Variant]> | null | undefined;
-  };
-  type ConfigVariantsMulti<T extends ConfigSchema> = {
-    [Variant in keyof T]?:
-      | StringToBoolean<keyof T[Variant]>
-      | StringToBoolean<keyof T[Variant]>[]
-      | undefined;
-  };
+    const getVariantClassNames = Object.keys(variants).map(
+      (variant: keyof typeof variants) => {
+        const variantProp = props?.[variant as keyof typeof props];
+        const defaultVariantProp = defaultVariants?.[variant];
 
-  type Config<T> = T extends ConfigSchema
-    ? {
-        variants?: T;
-        defaultVariants?: ConfigVariants<T>;
-        compoundVariants?: (T extends ConfigSchema
-          ? (ConfigVariants<T> | ConfigVariantsMulti<T>) & ClassProp
-          : ClassProp)[];
+        if (variantProp === null) return null;
+
+        const variantKey = (falsyToString(variantProp) ||
+          falsyToString(
+            defaultVariantProp
+          )) as keyof typeof variants[typeof variant];
+
+        return variants[variant][variantKey];
       }
-    : never;
+    );
 
-  type Props<T> = T extends ConfigSchema
-    ? ConfigVariants<T> & ClassProp
-    : ClassProp;
-
-  const cva =
-    <T>(base?: ClassValue, config?: Config<T>) =>
-    (props?: Props<T>) => {
-      if (config?.variants == null)
-        return cx(base, props?.class, props?.className);
-
-      const { variants, defaultVariants } = config;
-
-      const getVariantClassNames = Object.keys(variants).map(
-        (variant: keyof typeof variants) => {
-          const variantProp = props?.[variant as keyof typeof props];
-          const defaultVariantProp = defaultVariants?.[variant];
-
-          if (variantProp === null) return null;
-
-          const variantKey = (falsyToString(variantProp) ||
-            falsyToString(
-              defaultVariantProp
-            )) as keyof typeof variants[typeof variant];
-
-          return variants[variant][variantKey];
-        }
-      );
-
-      const propsWithoutUndefined =
-        props &&
-        Object.entries(props).reduce((acc, [key, value]) => {
-          if (value === undefined) {
-            return acc;
-          }
-
-          acc[key] = value;
+    const propsWithoutUndefined =
+      props &&
+      Object.entries(props).reduce((acc, [key, value]) => {
+        if (value === undefined) {
           return acc;
-        }, {} as Record<string, unknown>);
+        }
 
-      const getCompoundVariantClassNames = config?.compoundVariants?.reduce(
-        (
-          acc,
-          { class: cvClass, className: cvClassName, ...compoundVariantOptions }
-        ) =>
-          Object.entries(compoundVariantOptions).every(([key, value]) =>
-            Array.isArray(value)
-              ? value.includes(
-                  {
-                    ...defaultVariants,
-                    ...propsWithoutUndefined,
-                  }[key]
-                )
-              : {
+        acc[key] = value;
+        return acc;
+      }, {} as Record<string, unknown>);
+
+    const getCompoundVariantClassNames = config?.compoundVariants?.reduce(
+      (
+        acc,
+        { class: cvClass, className: cvClassName, ...compoundVariantOptions }
+      ) =>
+        Object.entries(compoundVariantOptions).every(([key, value]) =>
+          Array.isArray(value)
+            ? value.includes(
+                {
                   ...defaultVariants,
                   ...propsWithoutUndefined,
-                }[key] === value
-          )
-            ? [...acc, cvClass, cvClassName]
-            : acc,
-        [] as ClassValue[]
-      );
+                }[key]
+              )
+            : {
+                ...defaultVariants,
+                ...propsWithoutUndefined,
+              }[key] === value
+        )
+          ? [...acc, cvClass, cvClassName]
+          : acc,
+      [] as ClassValue[]
+    );
 
-      return cx(
-        base,
-        getVariantClassNames,
-        getCompoundVariantClassNames,
-        props?.class,
-        props?.className
-      );
-    };
+    return cx(
+      base,
+      getVariantClassNames,
+      getCompoundVariantClassNames,
+      props?.class,
+      props?.className
+    );
+  };
 
   return {
     cva,
@@ -154,14 +202,3 @@ export const create = (options?: CreateOptions) => {
   ============================================ */
 
 export const { cva, cx } = create();
-
-export type CxOptions = Parameters<typeof cx>;
-export type CxReturn = ReturnType<typeof cx>;
-
-/* Public Types
-  ============================================ */
-
-export type VariantProps<Component extends (...args: any) => any> = Omit<
-  OmitUndefined<Parameters<Component>[0]>,
-  ClassPropKey
->;
