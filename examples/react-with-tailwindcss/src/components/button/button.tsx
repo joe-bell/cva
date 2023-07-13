d26b9a5fa0e1b0f48c2d1c9f62fe2d49b91b86d6
Joe Bell <7349341+joe-bell@users.noreply.github.com>
2023-07-13 14:06:24 +0300

    chore(react-with-tailwindcss): include boolean variant

--- examples/react-with-tailwindcss/src/components/button/button.tsx
+++ examples/react-with-tailwindcss/src/components/button/button.tsx
@@ -4,38 +4,52 @@ import { cva, type VariantProps } from "class-variance-authority";
 const button = cva("button", {
   variants: {
     intent: {
-      primary: [
-        "bg-blue-500",
-        "text-white",
-        "border-transparent",
-        "hover:bg-blue-600",
-      ],
-      secondary: [
-        "bg-white",
-        "text-gray-800",
-        "border-gray-400",
-        "hover:bg-gray-100",
-      ],
+      primary: ["bg-blue-500", "text-white", "border-transparent"],
+      secondary: ["bg-white", "text-gray-800", "border-gray-400"],
     },
     size: {
       small: ["text-sm", "py-1", "px-2"],
       medium: ["text-base", "py-2", "px-4"],
     },
+    disabled: {
+      false: null,
+      true: ["opacity-50", "cursor-not-allowed"],
+    },
   },
-  compoundVariants: [{ intent: "primary", size: "medium", class: "uppercase" }],
+  compoundVariants: [
+    {
+      intent: "primary",
+      disabled: false,
+      class: "hover:bg-blue-600",
+    },
+    {
+      intent: "secondary",
+      disabled: false,
+      class: "hover:bg-gray-100",
+    },
+    { intent: "primary", size: "medium", class: "uppercase" },
+  ],
   defaultVariants: {
+    disabled: false,
     intent: "primary",
     size: "medium",
   },
 });
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+  extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, "disabled">,
     VariantProps<typeof button> {}
 
 export const Button: React.FC<ButtonProps> = ({
   className,
   intent,
   size,
+  disabled,
   ...props
-}) => <button className={button({ intent, size, className })} {...props} />;
+}) => (
+  <button
+    className={button({ intent, size, disabled, className })}
+    disabled={disabled || undefined}
+    {...props}
+  />
+);
