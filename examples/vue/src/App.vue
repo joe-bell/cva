a399d36ab4445c85bde0e54aea09519c78535fed
Joe Bell <7349341+joe-bell@users.noreply.github.com>
2023-07-13 14:05:48 +0300

    chore(vue): include boolean variant

--- examples/vue/src/App.vue
+++ examples/vue/src/App.vue
@@ -3,6 +3,7 @@ import Button from "./components/Button.vue";
 
 const intents = [undefined, "primary", "secondary"] as const;
 const sizes = [undefined, "medium", "small"] as const;
+const isDisabled = [undefined, ""] as const;
 </script>
 
 <template>
@@ -10,21 +11,28 @@ const sizes = [undefined, "medium", "small"] as const;
     <thead>
       <tr>
         <th></th>
+        <th></th>
         <th v-for="intent in intents">
           {{ intent || "default" }}
         </th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="size in sizes">
-        <th scope="row">{{ size || "default" }}</th>
+      <template v-for="disabled in isDisabled">
+        <tr v-for="(size, index) in sizes">
+          <th v-if="index === 0" scope="rowgroup" rowspan="3">
+            {{ typeof disabled === "undefined" ? "enabled" : "disabled" }}
+          </th>
 
-        <td v-for="intent in intents">
-          <Button :intent="intent" :size="size"
-            >{{ intent || "default" }} button
-          </Button>
-        </td>
-      </tr>
+          <th scope="row">{{ size || "default" }}</th>
+
+          <td v-for="intent in intents">
+            <Button :disabled="disabled" :intent="intent" :size="size"
+              >{{ intent || "default" }} button
+            </Button>
+          </td>
+        </tr>
+      </template>
     </tbody>
   </table>
 </template>
