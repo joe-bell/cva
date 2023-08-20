9a552fac50c03717f7249650d0e9a05496c941c5
Joe Bell <7349341+joe-bell@users.noreply.github.com>
2023-08-20 09:18:37 +0300

    Switch to Vitest (#198)
    * chore: switch to vitest
    
    * fully remove jest
    
    * chore: publishConfig
    
    * chore: clean up tsconfig
    
    * chore: clean up package.json paths
    
    * chore: clean up files

--- packages/@cva/tailwindcss/src/index.test.ts
+++ packages/@cva/tailwindcss/src/index.test.ts
@@ -1,7 +1,7 @@
 import { cva, cx } from "./";
 
 describe("cx", () => {
-  describe.each<Parameters<typeof cx>>([
+  describe.each<[Parameters<typeof cx>[0], ReturnType<typeof cx>]>([
     ["bg-blue-300 bg-green-300", "bg-green-300"],
     [["bg-blue-300", "bg-red-100", "bg-green-300"], "bg-green-300"],
   ])("cx(%o)", (options, expected) => {
@@ -12,9 +12,14 @@ describe("cx", () => {
 });
 
 describe("cva", () => {
-  expect(cva({ base: "bg-blue-300 bg-green-300" })()).toBe("bg-green-300");
-
-  expect(cva({ base: ["bg-blue-300", "bg-red-100", "bg-green-300"] })()).toBe(
-    "bg-green-300"
-  );
+  describe.each<
+    [Parameters<typeof cva>[0], ReturnType<ReturnType<typeof cva>>]
+  >([
+    [{ base: "bg-blue-300 bg-green-300" }, "bg-green-300"],
+    [{ base: ["bg-blue-300", "bg-red-100", "bg-green-300"] }, "bg-green-300"],
+  ])("cva(%o)", (options, expected) => {
+    test(`returns ${expected}`, () => {
+      expect(cva(options)()).toBe(expected);
+    });
+  });
 });
