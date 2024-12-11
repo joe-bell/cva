ab67cbaa6145711ccdc8b8a8643ea46308e9bddf
Joe Bell <7349341+joe-bell@users.noreply.github.com>
2024-12-11 11:36:16 +0200

    chore: add beta examples

---
+++ examples/beta/react-with-css-modules/src/components/button/button.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { cva, type VariantProps } from "cva";
+
+import styles from "./button.module.css";
+
+const button = cva({
+  base: styles.base,
+  variants: {
+    intent: {
+      primary: styles.primary,
+      secondary: styles.secondary,
+    },
+    size: {
+      small: styles.small,
+      medium: styles.medium,
+    },
+    disabled: {
+      false: styles.enabled,
+      true: styles.disabled,
+    },
+  },
+  compoundVariants: [
+    { intent: "primary", size: "medium", className: styles.primaryMedium },
+  ],
+  defaultVariants: {
+    intent: "primary",
+    size: "medium",
+    disabled: false,
+  },
+});
+
+export interface ButtonProps
+  extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, "disabled">,
+    VariantProps<typeof button> {}
+
+export const Button: React.FC<ButtonProps> = ({
+  className,
+  intent,
+  size,
+  disabled,
+  ...props
+}) => (
+  <button
+    className={button({ intent, size, disabled, className })}
+    disabled={disabled || undefined}
+    {...props}
+  />
+);
---
+++ examples/beta/react-with-tailwindcss/src/components/button/button.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { cva, type VariantProps } from "cva";
+
+const button = cva({
+  base: "button",
+  variants: {
+    intent: {
+      primary: ["bg-blue-500", "text-white", "border-transparent"],
+      secondary: ["bg-white", "text-gray-800", "border-gray-400"],
+    },
+    size: {
+      small: ["text-sm", "py-1", "px-2"],
+      medium: ["text-base", "py-2", "px-4"],
+    },
+    disabled: {
+      false: null,
+      true: ["opacity-50", "cursor-not-allowed"],
+    },
+  },
+  compoundVariants: [
+    {
+      intent: "primary",
+      disabled: false,
+      class: "hover:bg-blue-600",
+    },
+    {
+      intent: "secondary",
+      disabled: false,
+      class: "hover:bg-gray-100",
+    },
+    { intent: "primary", size: "medium", class: "uppercase" },
+  ],
+  defaultVariants: {
+    disabled: false,
+    intent: "primary",
+    size: "medium",
+  },
+});
+
+export interface ButtonProps
+  extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, "disabled">,
+    VariantProps<typeof button> {}
+
+export const Button: React.FC<ButtonProps> = ({
+  className,
+  intent,
+  size,
+  disabled,
+  ...props
+}) => (
+  <button
+    className={button({ intent, size, disabled, className })}
+    disabled={disabled || undefined}
+    {...props}
+  />
+);
