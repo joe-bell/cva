67170ce6105180026ced13aadd5a568136b31096
Mathis Chambon <mchambon@vvfvillages.fr>
2026-06-24 11:02:08 +0200

    perf: migrate tailwind-merge and clsx packages to cnfast to improve performance

--- packages/class-variance-authority/src/types.ts
+++ packages/class-variance-authority/src/types.ts
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-import type * as CLSX from "clsx";
+import type * as CLSX from "cnfast";
 
 export type ClassPropKey = "class" | "className";
 
